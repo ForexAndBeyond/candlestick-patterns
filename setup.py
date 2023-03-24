59dde839c666182f7d9b14bb69c774327a97278f
Jeffrey Phung <jjphung@ucsd.edu>
2023-03-24 11:30:08 -0700

    turned into an installable package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='candlestick',
+    version='0.0.2',
+    author='Jeffrey Phung',
+    author_email='phung1043@yahoo.com',
+    description='Candlestick Patterns',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ForexAndBeyond/candlestick-patterns',
+    license='MIT',
+    packages=['candlestick', 'candlestick.patterns'],
+    install_requires=['pandas','requests'],
+)
